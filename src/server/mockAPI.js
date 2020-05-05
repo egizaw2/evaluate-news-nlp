6d21359875da5a65cf24565102df0a4f1f731161
Eskinder Gizaw <54922276+egizaw2@users.noreply.github.com>
2020-05-05 13:55:42 -0400

    Update mockAPI.js

--- src/server/mockAPI.js
+++ src/server/mockAPI.js
@@ -1,12 +1,8 @@
 let json = {
-	title: "test json response",
-	message: "this is a message",
-	time: "now",
-};
+	'title': "test json response",
+	'message': "this is a message",
+	'time': "now",
+}
 
 module.exports = json;
-// set aylien API credentias
-var textapi = new aylien({
-	application_id: process.env.API_ID,
-	application_key: process.env.API_KEY
-});
+
