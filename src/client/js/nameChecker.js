10bd2e80f36c36a0d16b28d2fed3856a7c535194
Eskinder Gizaw <54922276+egizaw2@users.noreply.github.com>
2020-05-05 13:51:22 -0400

    Update nameChecker.js

--- src/client/js/nameChecker.js
+++ src/client/js/nameChecker.js
@@ -1,10 +1,10 @@
-function checkForName(inputText) {
-	console.log("::: Running checkForName :::", inputText);
-	let names = ["Picard", "Janeway", "Kirk", "Archer", "Georgiou"];
-
-	if (names.includes(inputText)) {
-		alert("Welcome, Captain!");
-	}
-}
-
+const checkForName = (url) => {
+	console.log(":::Running nameChecker :::", url)
+    var pattern = new RegExp('^(https?:\\/\\/)?'+ '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+      '((\\d{1,3}\\.){3}\\d{1,3}))'+ '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+      '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+      '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+    return !!pattern.test(url);
+  }
 export { checkForName };
+
