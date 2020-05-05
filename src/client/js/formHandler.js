06991de3b3442fec32a56a2083c57f2bcc1f8cc7
Eskinder Gizaw <54922276+egizaw2@users.noreply.github.com>
2020-05-05 13:49:38 -0400

    Update formHandler.js

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,16 +1,35 @@
- function handleSubmit(event) {
+import { checkForName } from './nameChecker' 
+
+function handleSubmit(event) {
 	event.preventDefault();
 
 	// check what text was put into the form field
-	let formText = document.getElementById("name").value;
-	Client.checkForName(formText);
+	let url = document.getElementById('url').value
+	//Client.checkForName(formText);
+	if (!checkForName(url)){
+		console.log('not valid url');
+		return;
+	}
 
-	console.log("::: Form Submitted :::");
-	fetch("http://localhost:8080/test")
-		.then((res) => res.json())
-		.then(function (res) {
-			document.getElementById("results").innerHTML = res.message;
-		});
+console.log("::: Form Submitted :::")
+	fetch('//localhost:8082/analysis', {
+		method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+		body: JSON.stringify({url})
+})
+	.then(res => res.json())
+	.then(function (res) {
+		document.getElementById('article').innerHTML = res.text;
+        document.getElementById('polarity').innerHTML = res.polarity;
+        document.getElementById('polarity_confidence').innerHTML = res.polarity_confidence;
+        document.getElementById('subjectivity').innerHTML = res.subjectivity;
+    document.getElementById('subjectivity_confidence').innerHTML = res.subjectivity_confidence;
+
+		//console.log(res);
+		//alert(dataText);
+	})
 }
 
-export { handleSubmit };
+export { handleSubmit }
