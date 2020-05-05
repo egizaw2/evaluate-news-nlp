3eeac0710fb56bd93dfbe1d253e2505a644e4c45
Eskinder Gizaw <54922276+egizaw2@users.noreply.github.com>
2020-05-05 13:59:35 -0400

    Update webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,6 +5,7 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
 	mode: "production",
@@ -12,7 +13,10 @@ module.exports = {
 	optimization: {
         minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
     },
-	
+	output: {
+        libraryTarget: 'var',
+        library: 'Client'
+      },
 	
 	module: {
 		rules: [
@@ -33,6 +37,7 @@ module.exports = {
 			filename: "./index.html",
 		}),
 
-		new MiniCssExtractPlugin({filename: '[name].css'}),
+		new MiniCssExtractPlugin({ filename: '[name].css' }),
+		new WorkboxPlugin.GenerateSW()
 	],
 };
