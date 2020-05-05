153aab32859aad133330533ba6f36e3741757895
Eskinder Gizaw <54922276+egizaw2@users.noreply.github.com>
2020-05-05 13:54:31 -0400

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -1,27 +1,52 @@
+const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 dotenv.config();
-console.log(`Your API key is ${process.env.API_KEY}`);
-var path = require("path");
-const express = require("express");
-const mockAPIResponse = require("./mockAPI.js");
-var aylien = require("aylien_textapi");
+//console.log(`Your API key is ${process.env.API_KEY}`);
+var path = require("path")
+const express = require("express")
+const mockAPIResponse = require("./mockAPI.js")
+var aylien = require("aylien_textapi")
+const cors = require('cors');
 
-const app = express();
+const textapi = new aylien({
+	application_id: process.env.API_ID,
+	application_key: process.env.API_KEY
+	});
 
-app.use(express.static("dist"));
+const app = express()
+app.use(cors())
+app.use(express.static("dist"))
 
-console.log(__dirname);
-
-app.get("/", function (req, res) {
+//console.log(__dirname);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.get('/', function (req, res) {
 	 res.sendFile('dist/index.html')
 	//res.sendFile(path.resolve("src/client/views/index.html"));
-});
+})
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-	console.log("Example app listening on port 8080!");
+app.listen(8082, function () {
+	console.log("Example app listening on port 8082!");
 });
 
-app.get("/test", function (req, res) {
+app.get('/test', function (req, res) {
 	res.send(mockAPIResponse);
-});
+})
+
+//POST 
+app.post('/analysis', (req, res) => {
+textapi.sentiment({ url: req.body.url
+		}, (error, result) =>{
+			if (error) {
+				console.log('error')
+				res.send();
+				return;
+			}
+		
+	console.log ('success') 
+		res.send(result);
+	})
+	});
+
+ module.exports = app;
